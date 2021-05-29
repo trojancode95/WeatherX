0890f8883ad64481dafe28b96ab5f3e6515b0354
trojancode95 <akhileshecenitjsr@gmail.com>
2021-05-29 13:20:41 +0530

    Weather-X App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const API_KEY = "8a45771001cd895140c377770ff1fe2d";
+
+function App() {
+  return <div className="App">Weather App</div>;
+}
+
+export default App;
