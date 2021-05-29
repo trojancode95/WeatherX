62377d9e59f542d586e4ddba873404c585b416c2
trojancode95 <akhileshecenitjsr@gmail.com>
2021-05-30 01:32:36 +0530

    Weather X App

--- README.md
+++ README.md
@@ -1,71 +1,34 @@
-# Getting Started with Create React App
+<h1 align="center">Weather-X</h1>
+<p align="center"><b>Weather is Yours to know! ⛅️</b></p>
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+- Developed this App to search Weather Conditions in **ANY CITY**. 
+- Used OpenWeatherAPI's functionality and integrated it into ReactJS. 
+- Beautified using *Bootstrap CSS Framework*.
 
-In the project directory, you can run:
+### Installation Guide
+```
+1. Open the terminal or git bash on your PC.
+```
 
-### `npm start`
+```
+2. Clone the repository `git clone https://github.com/trojancode95/WeatherX.git`
+```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+```
+3. Go inside the project directory.
+```
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+```
+4. Install the dependencies using `npm install`
+```
 
-### `npm test`
+```
+5. Start React dev server with `npm start`
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+##### The project has started on `localhost:3000` !!
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-# WeatherX
+---
+<p align="center"> Made with ❤️ by <a href="https://trojancode.me">Akhilesh</a></p>
--- src/App.js
+++ src/App.js
@@ -1,10 +1,141 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Form from "./app_component/form";
+import Weather from "./app_component/weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// git project https://github.com/trojancode95/WeatherX
+import "weather-icons/css/weather-icons.css";
 
 const API_KEY = "8a45771001cd895140c377770ff1fe2d";
 
-function App() {
-  return <div className="App">Weather App</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+
+    // setting state to empty initially
+    this.state = {
+      city: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      wind_speed: "",
+      error: false,
+    };
+
+    // setting weather to their respective icons
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
+    };
+  }
+
+  // Switch statements specified to get the specific icon for that specific weather condition
+  get_WeatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
+    }
+  }
+
+  // Converting the Temperature to the default - Celsius
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  // event handler for onSubmit button in the form component
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    // Retrieve the value in 'city' from the Textbox
+    const city = e.target.elements.city.value;
+
+    // When city is entered, the data is fetched and retrieved from the OpenWeatherAPI
+    if (city) {
+      const api_call = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+
+      // The response received is stored in the form of Standard JSON format
+      const response = await api_call.json();
+
+      // Values are retrieved from the API and Specified to the state
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        main: response.weather[0].main,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        wind_speed: response.wind.speed,
+        error: false,
+      });
+
+      // Icons are set
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+
+      // Response is console logged to check if it is working, if not, then the error function is thrown
+      console.log(response);
+    } else {
+      this.setState({
+        error: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App-Body">
+        <h1 className="heading"> WeatherX </h1>
+        <div className="App">
+          <Form loadweather={this.getWeather} error={this.state.error} />
+          <Weather
+            cityname={this.state.city}
+            weatherIcon={this.state.icon}
+            temp_celsius={this.state.celsius}
+            temp_max={this.state.temp_max}
+            temp_min={this.state.temp_min}
+            description={this.state.description}
+            wind_speed={this.state.wind_speed}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// Repsonsive
+// Suggestions
+// Dynamic -> Background Color -> Animations
